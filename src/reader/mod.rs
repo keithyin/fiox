72daf08e04f8ceaffaf9b569bd6e7713b5ef9b06
keithyin <yinpenghhz@hotmail.com>
2025-12-12 22:10:25 +0800

    linux check ok

--- src/reader/mod.rs
+++ src/reader/mod.rs
@@ -12,19 +12,33 @@ pub use sequential_reader_linux::SequentialReader;
 
 #[cfg(test)]
 mod test {
+    use std::{fs, io::{Read, Seek}};
+
     use super::SequentialReader;
 
     #[test]
     fn test_sequential_reader() {
-        let mut reader = SequentialReader::new("test_data/test_data.txt", 0, 4096, 2).unwrap();
-        let mut buf = vec![0_u8; 112560];
+        let read_start_pos = 10;
+        let mut reader = SequentialReader::new("test_data/test_data.txt", read_start_pos, 4096, 2).unwrap();
+        let mut reader2 = fs::File::open("test_data/test_data.txt").unwrap();
+        reader2.seek(std::io::SeekFrom::Start(read_start_pos)).unwrap();
+        let file_size = fs::metadata("test_data/test_data.txt").unwrap().len();
+        
+        let buf_size = 112560;
+        let mut buf = vec![0_u8; buf_size];
+        let mut buf2 = vec![0_u8; buf_size];
+        let mut read_size = 0;
         loop {
             let n = reader.read2buf(&mut buf).unwrap();
+            reader2.read_exact(&mut buf2[..n]).unwrap();
+            assert_eq!(&buf[..n], &buf2[..n]);
+            read_size += n as u64;
             if n == 0 {
                 break;
             }
-            print!("{}", String::from_utf8((&buf[..n]).to_vec()).unwrap());
+            // print!("{}", String::from_utf8((&buf[..n]).to_vec()).unwrap());
         }
-        println!("")
+        assert_eq!(read_size, file_size - read_start_pos);
+        println!("");
     }
 }
