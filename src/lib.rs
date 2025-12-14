546b3af3fa4e964aa6e19c12525cddffe22e38bf
rocyin <yinpenghhz@hotmail.com>
2025-12-14 11:52:41 +0800

    writer

--- src/lib.rs
+++ src/lib.rs
@@ -1,4 +1,5 @@
 pub mod reader;
+pub mod writer;
 
 pub fn add(left: u64, right: u64) -> u64 {
     left + right
---
+++ src/writer/mod.rs
@@ -0,0 +1,3 @@
+pub mod sequential_writer_linux;
+pub mod sequential_writer_windows;
+
