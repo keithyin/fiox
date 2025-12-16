28bcda114f29b3861bc527ba9b33ecaff96d9dc5
keith <ke1thyin@outlook.com>
2025-12-16 07:53:44 +0000

    test write

--- src/bin/test_write.rs
+++ src/bin/test_write.rs
@@ -1,11 +1,17 @@
 use fiox::SequentialWriter;
 
 fn main() {
-    let fpath = "E:/datas/fiox_write.bin";
+
+    let args = std::env::args().collect::<Vec<String>>();
+    assert!(args.len() > 2);
+    let fpath = args[1].trim();
+    let n_gbytes = args[2].parse::<usize>().unwrap();
+
+    // let fpath = "E:/datas/fiox_write.bin";
     let mut writer = SequentialWriter::new(fpath, 0, 1024 * 1024, 8).unwrap();
 
     let data = b"1234567890abcdefghijklmnopqrstuvwxyz\n";
-    for _ in 0..(1024 * 1024 * 1024 * 100 / data.len()) {
+    for _ in 0..(1024 * 1024 * 1024 * n_gbytes / data.len()) {
         writer.write(data).unwrap();
     }
 }
